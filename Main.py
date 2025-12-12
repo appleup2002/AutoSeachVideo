e6708c6b8601d326f0396ecb827361ac10819558
appleup2002 <appleup2002@gmail.com>
2025-12-12 14:22:29 +0800

    [feat]: 收藏夹信息获取完成

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import re
 import requests
 import json
+import time
+import random
 
 def getCookie():
     cookie = "123"
@@ -26,13 +28,32 @@ def getMid(datas):
     result = json.loads(result.text)
     return result['data']['mid'];
 
+def getFavFolder(mid, datas):
+    url = 'https://api.bilibili.com/x/v3/fav/folder/created/list-all'
+    headers = {
+        'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0 Safari/537.36',
+        'Cookie': 'SESSDATA=' + datas['SESSDATA'] + ';'
+    }
+    params = {
+        'up_mid': mid
+    }
+
+    result = requests.get(url = url, params= params, headers = headers)
+    result = json.loads(result.text)
+    return result["data"]['list']
+
 
 def main():
     cookie = getCookie()
     datas = getData(cookie)
     mid = getMid(datas)
-    print(mid)
-
+    time.sleep(random.randint(2,6))
+    folders = getFavFolder(mid, datas)
+    print("输入目标文件夹序号进行选择")
+    for i in range(0, len(folders)):
+        print(f'{i}. {folders[i]["title"]}')
+    target = int(input())
+    folder = folders[target]["id"]
 
 if __name__ == "__main__":
     main()
